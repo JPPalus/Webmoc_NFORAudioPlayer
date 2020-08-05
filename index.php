1ef006ba3d5a0c74fa9a6985bbb7887a6a6f75d8
JP Palus <mal@malekith.host>
2020-08-05 16:40:07 +0200

    maj

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $allow_direct_link = true; // Set to false to only allow downloads and not direc
 $allow_show_folders = true; // Set to false to hide all subdirectories
 
 $disallowed_patterns = ['*.php'];  // must be an array.  Matching files not allowed to be uploaded
-$allowed_patterns = ['*.mp3', '*.wav', '*.flac', '*.ogg']; // Matching files hidden in directory index
+$allowed_patterns = ['*.mp3', '*.wav', '*.flac', '*.ogg', '*/']; // Matching files hidden in directory index
 
 $PASSWORD = '';  // Set the password, to access the file manager... (optional)
 
@@ -107,7 +107,7 @@ function is_entry_allowed($entry, $allow_show_folders, $allowed_patterns) {
 		return false;
 	}
 	
-	if (is_dir($entry) && !$allow_show_folders) {
+	if (is_dir($entry) && $allow_show_folders) {
 		return true;
 	}
 	

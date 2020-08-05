97ecaa359e46d3568c95cb74c9f314d3af7828b1
JP Palus <mal@malekith.host>
2020-08-05 16:49:48 +0200

    maj

--- index.php
+++ index.php
@@ -259,21 +259,6 @@ function get_absolute_path($path) {
 	return implode(DIRECTORY_SEPARATOR, $absolutes);
 }
 
-function err($code,$msg) {
-	http_response_code($code);
-	header("Content-Type: application/json");
-	echo json_encode(['error' => ['code'=>intval($code), 'msg' => $msg]]);
-	exit;
-}
-
-function asBytes($ini_v) {
-	$ini_v = trim($ini_v);
-	$s = ['g'=> 1<<30, 'm' => 1<<20, 'k' => 1<<10];
-	return intval($ini_v) * ($s[strtolower(substr($ini_v,-1))] ?: 1);
-}
-
-$MAX_UPLOAD_SIZE = min(asBytes(ini_get('post_max_size')), asBytes(ini_get('upload_max_filesize')));
-
 ?>
 
 <!DOCTYPE html>

6a5da706145244ad75625379d37faf85639816a3
JP Palus <mal@malekith.host>
2020-08-05 18:42:03 +0200

    maj

--- index.php
+++ index.php
@@ -114,11 +114,11 @@ function is_entry_allowed($entry, $allow_show_folders, $allowed_patterns, $hidde
 		return false;
 	}
 	
-	foreach($allowed_patterns as $pattern) {
-		if(fnmatch($pattern, $entry)) {
-			return true;
-		}
-	}
+	// foreach($allowed_patterns as $pattern) {
+	// 	if(fnmatch($pattern, $entry)) {
+	// 		return true;
+	// 	}
+	// }
 	
 	return false;
 }

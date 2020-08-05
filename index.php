9b961bb19092934d7c09cb61b5a2bb836f405079
JP Palus <mal@malekith.host>
2020-08-05 18:21:46 +0200

    maj

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ function is_entry_allowed($entry, $allow_show_folders, $allowed_patterns, $hidde
 		// } else {
 		// 	return true;
 		// }
-		true;
+		return true;
 	}
 	
 	foreach($allowed_patterns as $pattern) {
